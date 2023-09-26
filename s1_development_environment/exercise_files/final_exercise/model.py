76b4c38c96f04cfd9f3492db89fd9c45eb1e3e9a
LennMarx <131344012+LennMarx@users.noreply.github.com>
2023-09-26 10:55:26 +0200

    fileimport from different repo

--- s1_development_environment/exercise_files/final_exercise/model.py
+++ s1_development_environment/exercise_files/final_exercise/model.py
@@ -4,4 +4,17 @@ from torch import nn
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 128)
+        self.conv1 = nn.Conv2d(1,32, kernel_size=3)
+        self.ReLU1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(32,32,kernel_size=3)
+        self.ReLU2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.flat = nn.Flatten()
+        self.fc1 = nn.Linear(800,10)
+
+    def forward(self,x):
+        x = self.ReLU1(self.pool1(self.conv1(x)))
+        x = self.ReLU2(self.pool2(self.conv2(x)))
+        return self.fc1(self.flat(x))
+
